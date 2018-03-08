dc1bf76b9428a086bba42d1d61ae2a54e3e85e4b
Henricus Basien <Henricus@Basien.de>
2018-03-08 12:30:09 +0100

    Added Kivy Installer Support for all environments

--- Installer/Kivy_Installer.sh
+++ Installer/Kivy_Installer.sh
@@ -38,6 +38,38 @@ PrintSubHeader "Installing Required Programs"
 Run "sudo $apt update"
 Run "$pipInstall cython"
 
+# Install necessary system packages
+sudo apt-get install -y \
+    python-pip \
+    build-essential \
+    git \
+    python \
+    python-dev \
+    ffmpeg \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-ttf-dev \
+    libportmidi-dev \
+    libswscale-dev \
+    libavformat-dev \
+    libavcodec-dev \
+    zlib1g-dev
+
+# Install gstreamer for audio, video (optional)
+sudo apt-get install -y \
+    libgstreamer1.0 \
+    gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-good
+
+#Note: Depending on your Linux version, you may receive error messages related to the “ffmpeg” package. In this scenario, use “libav-tools ” in place of “ffmpeg ” (above), or use a PPA (as shown below):
+
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+sudo apt-get install ffmpeg
+
+
+
 #--------------------------------------------------------------
 PrintSubHeader "Installing Kivy"
 #--------------------------------------------------------------
