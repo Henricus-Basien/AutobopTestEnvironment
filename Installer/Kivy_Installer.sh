9628b399c7f9d99abbac1f7c257602f049afaa70
Henricus Basien <Henricus@Basien.de>
2018-03-08 12:20:05 +0100

    Added Kivy Installer

---
+++ Installer/Kivy_Installer.sh
@@ -0,0 +1,53 @@
+Kivy_Header='
+ ____  __.__              
+|    |/ _|__|__  _____.__.
+|      < |  \  \/ <   |  |
+|    |  \|  |\   / \___  |
+|____|__ \__| \_/  / ____|
+        \/         \/     
+.___                 __         .__  .__                       
+|   | ____   _______/  |______  |  | |  |   ___________        
+|   |/    \ /  ___/\   __\__  \ |  | |  | _/ __ \_  __ \       
+|   |   |  \\\\___ \  |  |  / __ \|  |_|  |_\  ___/|  | \/       
+|___|___|  /____  > |__| (____  /____/____/\___  >__|          
+         \/     \/            \/               \/              
+'
+printf "${Kivy_Header}"
+
+printf "Created on 08.03.2018 by Henricus N. Basien (Henricus@Basien.de)"
+
+#=================================================================================
+# Initialization
+#=================================================================================
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/Installer_Init.sh"
+
+#=================================================================================
+PrintHeader "Installation"
+#=================================================================================
+
+echo "Please fill in your sudo Password to install Kivy: "
+sudo echo "Thank you!"
+
+#--------------------------------------------------------------
+PrintSubHeader "Installing Required Programs"
+#--------------------------------------------------------------
+
+Run "sudo $apt update"
+Run "$pipInstall cython"
+
+#--------------------------------------------------------------
+PrintSubHeader "Installing Kivy"
+#--------------------------------------------------------------
+
+ConfirmQ="Do you want to (re-)install Kivy [Y/n]?"
+result=$(Confirm "$ConfirmQ")
+if $result; then
+	Run "$aptInstall_Y python-kivy"
+else
+    Warning "Kivy is not being installed!"
+fi
+
+
